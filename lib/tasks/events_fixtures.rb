dec969a825a1de799fa5b741dbaa6f3142a9bd28
momotofu <mrkita.reece@gmail.com>
2021-01-23 03:22:44 +0900

    Added asset_types and sub_types to events_fixtures

--- lib/tasks/events_fixtures.rb
+++ lib/tasks/events_fixtures.rb
@@ -42,5 +42,52 @@ module EventsFixtures
     },
   ].freeze
 
-  ASSET_TYPES = []
+  ASSET_TYPES = {
+    account: ["outcomes"],
+    assignment: [],
+    calendar_event: [],
+    course: [
+      "assignments",
+      "files",
+      "grades",
+      "conferences",
+      "syllabus",
+      "pages",
+      "modules",
+      "roster",
+      "calendar_feed",
+      "home",
+      "outcomes",
+      "speed_grader",
+      "announcements",
+      "topics",
+      "collaborations",
+      "quizzes",
+    ],
+    enrollment: [],
+    group: [
+      "conferences",
+      "calendar_feed",
+      "files",
+      "pages",
+      "announcements",
+      "topics",
+      "roster",
+      "collaborations",
+    ],
+    learning_outcome: [],
+    user: [
+      "calendar_feed",
+      "files",
+    ],
+    collaboration: [],
+    "quizzes:quiz": [],
+    context_external_tool: [], # (this type is used to identify all LTI versions except LTI 2.0 launches)
+    "lti/tool_proxy": [], # (this type is used to identify LTI 2.0 launches)
+    web_conference: [],
+    wiki_page: [],
+    content_tag: [],
+    discussion_topic: [],
+    attachment: [],
+  }
 end
